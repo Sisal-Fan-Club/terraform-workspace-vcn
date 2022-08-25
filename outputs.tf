8353c0b6ea2df2711919877ad21660c2a9b4ac22
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 14:23:23 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,15 @@ output "dmz_route_table" {
   
   sensitive = true
 }
+
+output "nat_gateway" {
+  value = local.internet_gw
+  
+  sensitive = true
+}
+
+output "app_route_table" {
+  value = local.app_route_table
+  
+  sensitive = true
+}
