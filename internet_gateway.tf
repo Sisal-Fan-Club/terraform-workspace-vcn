57bd88ca1d4597f01341df70a13c6c734072e369
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 13:43:13 +0200

    Update internet_gateway.tf

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -6,7 +6,7 @@ resource "oci_core_internet_gateway" "internet_gateway" {
   compartment_id = local.vcn.compartment_id
   vcn_id = local.vcn.id
   
-  display_name = "Internet Gateway to expose public IPs(NAT 1:1)"
+  display_name = "Internet Gateway to expose public IPs (DNAT)"
   
   freeform_tags = merge({
     sercurity-profile = "dmz"
