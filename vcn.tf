066c862a341fd41b51bbfe9ade6a16a7d3bcd96e
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 13:07:44 +0200

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -1,2 +1,15 @@
+locals {
+  root_compartment = local.tfe_workspace_outputs.root_compartment.root_compartment
+}
+
 resource "oci_core_vcn" "vcn" {
+  compartment_id = local.root_compartment.id
+  
+  display_name = "${local.root_compartment.description} - Virtual Cloud Network"
+  cidr_blocks = [
+    "10.0.0.0/16",
+  ]
+  
+  freeform_tags = merge({
+  }, local.root_compartment.freeform_tags)
 }
